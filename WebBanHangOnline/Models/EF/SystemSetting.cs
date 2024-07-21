61bca8eda0ee6ec8d277ab039519f98db8841492
vinh <vinh0962517230@gmail.com>
2024-07-21 21:07:20 +0700

    Add project files.

---
+++ WebBanHangOnline/Models/EF/SystemSetting.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace WebBanHangOnline.Models.EF
+{
+    [Table("tb_SystemSetting")]
+    public class SystemSetting
+    {
+        [Key]
+        [StringLength(50)]
+        //[DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
+        public string SettingKey {  get; set; }
+        [StringLength(4000)]
+        public string SettingValue {  get; set; }
+        [StringLength(4000)]
+        public string SettingDescrtipion {  get; set; }
+    }
+}
